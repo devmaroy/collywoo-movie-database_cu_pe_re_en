29ecead32e5b1402d53114f9d949faaa1b535ab5
Marek Matejovic <me@devmaroy.com>
2021-08-30 18:25:03 +0200

    [CMD-1] Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: ["./dist/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          900: "#04080f",
+          800: "#080d16",
+          600: "#0e131e",
+          500: "#3c434f",
+          400: "#737c8d",
+          100: "#acb3c0",
+        },
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+        white: "#dae3e5",
+        whiteOff: "#dae3e5",
+        whiteLight: "#a1a9b8",
+        primaryOff: {
+          900: "#507dbc",
+          800: "#a1c6ea",
+          700: "#bbd1ea",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
