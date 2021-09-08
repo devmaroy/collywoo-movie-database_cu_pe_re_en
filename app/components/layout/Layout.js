9118a39d35db12cb1b3e0577686aa43ca22abbb0
Marek Matejovic <me@devmaroy.com>
2021-09-08 15:02:49 +0200

    [CMD-16] Implemented pagination

--- app/components/layout/Layout.js
+++ app/components/layout/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { node } from 'prop-types';
 import Header from './Header';
-import Footer from './Footer';
+// import Footer from './Footer';
 
 const Layout = ({ children }) => (
-  <div className="pr-6 pl-6 mt-16 max-w-screen-xl mx-auto flex flex-col justify-start min-h-screen">
+  <div className="pr-6 pl-6 mt-16 max-w-screen-xl mx-auto fdlex flexd-col justify-sdtart min-dh-screen mb-80">
     <Header />
     <main>{children}</main>
-    <Footer />
+    {/* <Footer /> */}
   </div>
 );
 
