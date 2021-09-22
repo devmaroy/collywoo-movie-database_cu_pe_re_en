d9254b53cbfda2ccc55dce47d4d3dc1451609911
Marek Matejovic <me@devmaroy.com>
2021-09-22 16:56:07 +0200

    [CMD-23] Fixed .env for production

--- app/hooks/useLocalStorage.js
+++ app/hooks/useLocalStorage.js
@@ -3,7 +3,7 @@ import { useState, useCallback } from 'react';
 const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      // Try to get value from localStorage if exists
+      // Try to get value from localStorage if exists.
       // If now just use a initial value passed to the hook.
       const item = localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
